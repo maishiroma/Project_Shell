9d27bca4dae0662bd43e127a0b25f30e2b6b4588
shiro105 <shiro105@mail.chapman.edu>
2019-07-01 21:57:53 -0700

    Added shell movement

--- Project_Shell/Assets/Scripts/GameManager.cs
+++ Project_Shell/Assets/Scripts/GameManager.cs
@@ -7,9 +7,18 @@ using UnityEngine;
 
 namespace MattScripts {
 
+    public enum GameState {
+        SELECTING,
+        SHUFFLING,
+        START
+    }
+
     public class GameManager : MonoBehaviour {
 
         public static GameManager Instance;
+
+        public GameState currentState = GameState.START;
+        public int numberOfSwitches = 10;                   // Number of times the shells swap
 
         // Singleton Pattern
 		private void Awake()
@@ -24,5 +33,83 @@ namespace MattScripts {
                 Destroy(gameObject);
             }
 		}
+
+        // We set the game state to be starting
+		private void Start()
+		{
+            currentState = GameState.START;
+		}
+
+		// When called, randomly picks a shell that is deemed the winning shell.
+		private void DetermineLuckyShell()
+        {
+            int randomIndex = Random.Range(0, InteractShell.shellList.Count);
+            InteractShell.shellList[randomIndex].isWinner = true;
+            print(InteractShell.shellList[randomIndex].name + " is the lucky one.");
+        }
+
+        // When called, resets all of the shells to their normal states and positions
+        private void ResetShells()
+        {
+            foreach(InteractShell currentShell in InteractShell.shellList)
+            {
+                currentShell.transform.position = currentShell.GetOrigLocation;
+
+                if(currentShell.isWinner == true)
+                {
+                    currentShell.isWinner = false;
+                    print(currentShell.name + " was reset back to normal.");
+                }
+            }
+        }
+
+        // While this is running, the shells will be called to move.
+        private IEnumerator PerformGame()
+        {
+            for(int iterating = 0; iterating < numberOfSwitches; ++iterating)
+            {
+                // We pick two random shells to move
+                int randomIndex1 = Random.Range(0, InteractShell.shellList.Count);
+                int randomIndex2 = Random.Range(0, InteractShell.shellList.Count);
+                while(randomIndex1 == randomIndex2)
+                {
+                    randomIndex2 = Random.Range(0, InteractShell.shellList.Count);
+                }
+
+                InteractShell currShell1 = InteractShell.shellList[randomIndex1];
+                InteractShell currShell2 = InteractShell.shellList[randomIndex2];
+                currShell1.SwapShellLocation(currShell2);
+
+                // While we are moving these shells, we wait
+                while(currShell1.IsMoving || currShell2.IsMoving)
+                {
+                    yield return null;
+                }
+            }
+            currentState = GameState.SELECTING;
+        }
+    
+        // When called, starts up the game
+        public void StartGame()
+        {
+            if(currentState == GameState.START)
+            {
+                ResetShells();
+                DetermineLuckyShell();
+                currentState = GameState.SHUFFLING;
+
+                StartCoroutine(PerformGame());
+            }
+        }
+    
+        // When called, resets the game.
+        public void ResetGame()
+        {
+            if(currentState == GameState.SELECTING)
+            {
+                ResetShells();
+                currentState = GameState.START;
+            }
+        }
     }
 }
--- Project_Shell/Assets/Scripts/InteractShell.cs
+++ Project_Shell/Assets/Scripts/InteractShell.cs
@@ -1,4 +1,6 @@
 ﻿/*  This defines how an object interacts with the mouse
+ * 
+ *  Documentation: http://luminaryapps.com/blog/arcing-projectiles-in-unity/
  */
 
 using System.Collections;
@@ -12,6 +14,22 @@ namespace MattScripts {
         public static List<InteractShell> shellList;    // All of the shells know how many shells are there in the game
 
         public bool isWinner = false;                   // This indicates that this shell is the lucky one
+        public float moveSpeed = 1f;                    // How fast does this object move?
+        public float arcHeight = 1f;                    // How high is the arch on this object's movement?
+
+        // Private Variables
+        private bool isMoving;                          // IS the object currently moving?
+        private Vector3 newLocation;                    // The new destination to move this object towards
+        private Vector3 origLocation;                   // The initial location of this object
+        private Vector3 startMoveLocation;              // The current location this object is at before moving
+
+        public bool IsMoving {
+            get {return isMoving;}
+        }
+
+        public Vector3 GetOrigLocation {
+            get { return origLocation; }
+        }
 
         // Adds this shell to the list of shells in the game
 		private void Awake()
@@ -24,43 +42,95 @@ namespace MattScripts {
             shellList.Add(this);
 		}
 
+        // Sets all of the variables to defaults
+		private void Start()
+		{
+            origLocation = gameObject.transform.position;
+            newLocation = Vector3.zero;
+            startMoveLocation = Vector3.zero;
+            isMoving = false;
+		}
+
+        // Handles movement of the objects
+		private void Update()
+		{
+            if(isMoving == true)
+            {
+                // This logic calculates the movement of the shell in a curve
+                float xDiff = newLocation.x - startMoveLocation.x;
+                float nextXPos = Mathf.MoveTowards(transform.position.x, newLocation.x, moveSpeed * Time.deltaTime);
+
+                // For these next two calculations, if we get a NaN, we set the value to 0
+                float newYPos = Mathf.Lerp(startMoveLocation.y, newLocation.y, (nextXPos - startMoveLocation.x) / xDiff);
+                if(float.IsNaN(newYPos))
+                {
+                    newYPos = 0;
+                }
+
+                float arc = arcHeight * (nextXPos - startMoveLocation.x) * (nextXPos - newLocation.x) / (-0.25f * xDiff * xDiff);
+                if(float.IsNaN(arc))
+                {
+                    arc = 0;
+                }
+
+                // We then move the object to this position
+                gameObject.transform.position = new Vector3(nextXPos, newYPos + arc, 0);
+                if(gameObject.transform.position == newLocation)
+                {
+                    // Once we hit the new location, we stop moving
+                    isMoving = false;
+                }
+            }
+		}
+
 		// We have selected this object
 		private void OnMouseUp()
 		{
-            Debug.Log("We selected " + name);
+            if(GameManager.Instance.currentState == GameState.SELECTING)
+            {
+                if(isWinner == true)
+                {
+                    Debug.Log("You win!");
+                }
+                else
+                {
+                    Debug.Log("You lose...");
+                }
+                GameManager.Instance.ResetGame();
+            }
 		}
 
         // We are hovering over said object
 		private void OnMouseEnter()
 		{
-            Debug.Log("We are over " + name);
-		}
+            if(GameManager.Instance.currentState == GameState.SELECTING)
+            {
+                Debug.Log("We selected " + name);
+            }		
+        }
 
         // We left the mouse from this object
 		private void OnMouseExit()
 		{
-            Debug.Log("We exited " + name);
-		}
-	
-        // When called, randomly picks a shell that is deemed the winning shell.
-        public void DetermineLuckyShell()
-        {
-            int randomIndex = Random.Range(0, shellList.Count);
-            shellList[randomIndex].isWinner = true;
-            print(shellList[randomIndex].name + " is the lucky one.");
-        }
-
-        // When called, resets all of the shells to their normal states
-        public void ResetShells()
-        {
-            foreach(InteractShell currentShell in shellList)
+            if(GameManager.Instance.currentState == GameState.SELECTING)
             {
-                if(currentShell.isWinner == true)
-                {
-                    currentShell.isWinner = false;
-                    print(currentShell.name + " was reset back to normal.");
-                    break;
-                }
+                Debug.Log("We selected " + name);
+            }		
+        }
+	
+        // Swaps this and the other shell's location
+        public void SwapShellLocation(InteractShell otherShell)
+        {
+            if(isMoving == false && otherShell.isMoving == false)
+            {
+                startMoveLocation = gameObject.transform.position;
+                otherShell.startMoveLocation = otherShell.transform.position;
+
+                newLocation = otherShell.startMoveLocation;
+                otherShell.newLocation = startMoveLocation;
+
+                isMoving = true;
+                otherShell.isMoving = true;
             }
         }
     }
