642dc30f5d88b392516794778de8a3a94cc7698a
shiro105 <shiro105@mail.chapman.edu>
2019-07-02 23:14:44 -0700

    Added basic UI controls

---
+++ Project_Shell/Assets/Scripts/GameUI.cs
@@ -0,0 +1,54 @@
+﻿/*  Handles all of the UI in the game
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace MattScripts {
+
+    public class GameUI : MonoBehaviour {
+
+        public static GameUI Instance;
+
+        public Button startButton;                      // Reference to the Start Button
+        public TextMeshProUGUI gameMessage;             // Reference to the game message
+
+        // Singleton Pattern
+        private void Awake()
+        {
+            if(Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        // Depending on the game state, this changes the game UI
+		private void Update()
+		{
+            switch(GameManager.Instance.currentState)
+            {
+                case GameState.START:
+                    startButton.gameObject.SetActive(true);
+                    gameMessage.text = "Welcome to Shell Game!";
+                    break;
+                case GameState.SHUFFLING:
+                    startButton.gameObject.SetActive(false);
+                    gameMessage.text = "Keep an eye on the prize!";
+                    break;
+                case GameState.SELECTING:
+                    startButton.gameObject.SetActive(false);
+                    gameMessage.text = "Which one is the lucky object?";
+                    break;
+            }
+		}
+	}
+}
